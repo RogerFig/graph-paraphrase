6de9e848188219557f005ea0da684d48b382fd5c
rafael <ranchieta@outlook.com>
2020-05-20 11:14:33 -0300

    Download pre-trained embeddings

--- download.sh
+++ download.sh
@@ -6,55 +6,55 @@ echo "Done!!!"
 
 echo "Downloading Word2Vec skip_s50"
 wget http://143.107.183.175:22980/download.php?file=embeddings/word2vec/skip_s50.zip -O skip_s50.zip
-unzip skip_s50.zip -d embeddings && mv skip_s50.txt word2vec_50.txt
+unzip skip_s50.zip -d embeddings && mv embeddings/skip_s50.txt embeddings/word2vec_50.txt
 rm skip_s50.zip
 echo "Done!!!"
 
 echo "Downloading Word2Vec skip_s100"
 wget http://143.107.183.175:22980/download.php?file=embeddings/word2vec/skip_s100.zip -O skip_s100.zip
-unzip skip_s100.zip -d embeddings && mv skip_s100.txt word2vec_100.txt
+unzip skip_s100.zip -d embeddings && mv embeddings/skip_s100.txt embeddings/word2vec_100.txt
 rm skip_s100.zip
 echo "Done!!!"
 
 echo "Downloading Word2Vec skip_s300"
 wget http://143.107.183.175:22980/download.php?file=embeddings/word2vec/skip_s300.zip -O skip_s300.zip
-unzip skip_s300.zip -d embeddings && mv skip_s300.txt word2vec_300.txt
+unzip skip_s300.zip -d embeddings && mv embeddings/skip_s300.txt embeddings/word2vec_300.txt
 rm skip_s300.zip
 echo "Done!!!"
 
 echo "Downloading FastText skip_s50"
 wget http://143.107.183.175:22980/download.php?file=embeddings/fasttext/skip_s50.zip -O skip_s50.zip
-unzip skip_s50.zip -d embeddings && mv skip_s50.txt fasttext_50.txt
+unzip skip_s50.zip -d embeddings && mv embeddings/skip_s50.txt embeddings/fasttext_50.txt
 rm skip_s50.zip
 echo "Done!!!"
 
 echo "Downloading FastText skip_s100"
 wget http://143.107.183.175:22980/download.php?file=embeddings/fasttext/skip_s100.zip -O skip_s100.zip
-unzip skip_s100.zip -d embeddings && mv skip_s100.txt fasttext_100.txt
+unzip skip_s100.zip -d embeddings && mv embeddings/skip_s100.txt embeddings/fasttext_100.txt
 rm skip_s100.zip
 echo "Done!!!"
 
 echo "Downloading FastText skip_s300"
 wget http://143.107.183.175:22980/download.php?file=embeddings/fasttext/skip_s300.zip -O skip_s300.zip
-unzip skip_s300.zip -d embeddings && mv skip_s300.txt fasttext_300.txt
+unzip skip_s300.zip -d embeddings && mv embeddings/skip_s300.txt embeddings/fasttext_300.txt
 rm skip_s300.zip
 echo "Done!!!"
 
 echo "Downloading Wang2Vec skip_s50"
 wget http://143.107.183.175:22980/download.php?file=embeddings/wang2vec/skip_s50.zip -O skip_s50.zip
-unzip skip_s50.zip -d embeddings && mv skip_s50.txt wang2vec_50.txt
+unzip skip_s50.zip -d embeddings && mv embeddings/skip_s50.txt embeddings/wang2vec_50.txt
 rm skip_s50.zip
 echo "Done!!!"
 
 echo "Downloading Wang2Vec skip_s100"
 wget http://143.107.183.175:22980/download.php?file=embeddings/wang2vec/skip_s100.zip -O skip_s100.zip
-unzip skip_s100.zip -d embeddings && mv skip_s100.txt wang2vec_100.txt
+unzip skip_s100.zip -d embeddings && mv embeddings/skip_s100.txt embeddings/wang2vec_100.txt
 rm skip_s100.zip
 echo "Done!!!"
 
 echo "Downloading Wang2Vec skip_s300"
 wget http://143.107.183.175:22980/download.php?file=embeddings/wang2vec/skip_s300.zip -O skip_s300.zip
-unzip skip_s300.zip -d embeddings && mv skip_s300.txt wang2vec_300.txt
+unzip skip_s300.zip -d embeddings && mv embeddings/skip_s300.txt embeddings/wang2vec_300.txt
 rm skip_s300.zip
 echo "Done!!!"
 
